3afa9a48129caaf0296ed19eb85118dd96b4bfac
QIKEXIN <87749356@qq.COM>
2018-06-12 00:17:08 +0800

    1

--- np_training.py
+++ np_training.py
@@ -5,3 +5,19 @@ b=a.reshape(3,4)
 print(b)
 print(b.ndim)
 print(b.itemsize)
+
+x=np.linspace(0,10,5,False)
+print(x)
+x=np.logspace(1,10,num=10,base=2)
+print(x)
+
+a = np.arange(10)
+s = slice(2,7,2)
+print(a)
+print(a[s])
+b = a[2:7:2]
+print(b)
+print(a[1::3])
+
+a = np.array([[1,2,3],[3,4,5],[4,5,6]])
+print(a[:,1:])
